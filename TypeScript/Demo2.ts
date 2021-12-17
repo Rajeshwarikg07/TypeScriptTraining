f57a2e6bb99074abe0927a040760396e17e7b3d8
Rajeshwarikg07 <96120269+Rajeshwarikg07@users.noreply.github.com>
2021-12-17 15:01:27 +0530

    Delete Demo2.ts

--- TypeScript/Demo2.ts
+++
@@ -1,7 +0,0 @@
-let username:string="Scott";
-let age:number=25;
-let isValidUser:boolean =true;
-
-console.log("User Name : " + username);
-console.log("User Age : " + age);
-console.log("Is Valid User : " + isValidUser);
