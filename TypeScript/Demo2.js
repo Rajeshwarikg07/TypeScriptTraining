dee352f2c8897c13a5c9dc9d107ab83f6407ac23
Rajeshwarikg07 <96120269+Rajeshwarikg07@users.noreply.github.com>
2021-12-17 15:01:18 +0530

    Delete Demo2.js

--- TypeScript/Demo2.js
+++
@@ -1,6 +0,0 @@
-var username = "Scott";
-var age = 25;
-var isValidUser = true;
-console.log("User Name : " + username);
-console.log("User Age : " + age);
-console.log("Is Valid User : " + isValidUser);
